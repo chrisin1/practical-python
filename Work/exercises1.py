76e81d89cffb8c5ac94f82cefaafdda798aae401
Chris In <christopherin12@gmail.com>
2020-08-18 20:03:48 -0400

    new examples

--- Work/exercises1.py
+++ Work/exercises1.py
@@ -13,16 +13,40 @@
 # reading a file
 import os
 # import urllib.request
-os.getcwd()
-with open('Data/portfolio.csv', 'rt') as f:
-    # data = f.read() #all data on one line
-    # skip headers
-    # headers = next(f)
-    # print (headers)
-    for line in f:
-        print(line, end = '') # one line at a time
+# os.getcwd()
+# with open('Data/portfolio.csv', 'rt') as f:
+#     # data = f.read() #all data on one line
+#     # skip headers
+#     # headers = next(f)
+#     # print (headers)
+#     for line in f:
+#         print(line, end = '') # one line at a time
 
 # fetching internet data
 # u = urllib.request.urlopen('http://www.python.org/')
 # data = u.read()
-  
+
+
+# working with sequences and iteration
+# iteration
+# points = [
+#   (1, 4, 1),(10, 40, 1),(23, 14, 2),(5, 6, 3),(7, 8, 4)
+# ]
+# for x, y in enumerate(points, start = 1):
+#     print ('Coord ' + str(x) + ": ", end =""),
+#     print(y, end = '')
+#     print("hi")  
+
+
+# a = [0,1,2,3,4,5,6,7,8]
+# b = a[2:5]
+# b[0] = 500
+# print(a)
+# print(b)
+
+# #zip function
+# columns = ['column1', 'column2', 'column3']
+# rows = ["row1", 'row2', 'row3']
+# pairs = zip(columns, rows)
+# for column, row in pairs:
+#     print(column + ' ' + row)
