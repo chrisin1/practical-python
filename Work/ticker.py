566bf9df089c797b32653231fb53dd62ee774752
Chris In <christopherin12@gmail.com>
2020-08-23 15:50:28 -0400

    6.15 - generator expressions

--- Work/ticker.py
+++ Work/ticker.py
@@ -2,7 +2,7 @@ from follow import follow
 import csv
 import report
 import tableformat
-
+import time
 def convert_types(rows, types):
     for row in rows:
         yield [func(val) for func, val in zip(types, row)]
@@ -22,25 +22,21 @@ def select_columns(rows, indices):
     for row in rows:
         yield [row[index] for index in indices]
 
-def filter_symbols(rows, names):
-    for row in rows:
-        if row['name'] in names:
-            yield row
-
-if __name__ == '__main__':
-    lines = follow('Data/stocklog.csv')
-    portfolio = report.read_portfolio('Data/portfolio.csv')
-    rows = parse_stock_data(lines)
-    rows = filter_symbols(rows, portfolio)
-    for row in rows:
-        print(row)
 
 def ticker(portfile, logfile, fmt):
     portfolio = report.read_portfolio(portfile)
     lines = follow(logfile)
     rows = parse_stock_data(lines)
-    rows = filter_symbols(rows, portfolio)
+    rows = (row for row in rows if row['name'] in portfolio)
     formatter = tableformat.create_formatter(fmt)
     formatter.headings(['Name','Price','Change'])
     for row in rows:
         formatter.row([ row['name'], f"{row['price']:0.2f}", f"{row['change']:0.2f}"] )
+
+def main(args):
+    if len(args) != 4:
+        raise SystemExit('Usage: %s portfoliofile logfile fmt' % args[0])
+    ticker(args[1], args[2], args[3])
+if __name__ == '__main__':
+    import sys
+    main(sys.argv)
