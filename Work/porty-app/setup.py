95d0ee79aa4479557eb2dbf60db50f7921d3c2bb
Chris In <christopherin12@gmail.com>
2020-08-26 11:20:52 -0400

    9.4 - package distribution

---
+++ Work/porty-app/setup.py
@@ -0,0 +1,11 @@
+# setup.py
+import setuptools
+
+setuptools.setup(
+    name="porty",
+    version="0.0.1",
+    author="Christopher In",
+    author_email="inc@ufl.edu",
+    description="Practical Python Code",
+    packages=setuptools.find_packages(),
+)
